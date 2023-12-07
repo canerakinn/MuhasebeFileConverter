cd9375cd7c8871badd37282adeebb420dd6292d1
canera <canera@ATANTB53>
2023-12-07 17:06:48 +0300

    Add project files.

---
+++ MuhasebeFileConverter/IslemDefteri.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace MuhasebeFileConverter
+{
+    partial class IslemDefteri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadTxtFile = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBoxAfk = new System.Windows.Forms.CheckedListBox();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.selectFormat = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uploadTxtFile
+            // 
+            this.uploadTxtFile.Location = new System.Drawing.Point(1015, 55);
+            this.uploadTxtFile.Name = "uploadTxtFile";
+            this.uploadTxtFile.Size = new System.Drawing.Size(437, 47);
+            this.uploadTxtFile.TabIndex = 0;
+            this.uploadTxtFile.Text = "Dosya Yükle\r\n ( .txt )";
+            this.uploadTxtFile.UseVisualStyleBackColor = true;
+            this.uploadTxtFile.Click += new System.EventHandler(this.uploadTxtFile_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(50, 55);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(935, 794);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(263, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 23);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1011, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ACENTE/FON KODU (AFK) : ";
+            // 
+            // checkedListBoxAfk
+            // 
+            this.checkedListBoxAfk.FormattingEnabled = true;
+            this.checkedListBoxAfk.Location = new System.Drawing.Point(1015, 144);
+            this.checkedListBoxAfk.Name = "checkedListBoxAfk";
+            this.checkedListBoxAfk.Size = new System.Drawing.Size(437, 497);
+            this.checkedListBoxAfk.TabIndex = 5;
+            this.checkedListBoxAfk.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBoxAfk_ItemCheck);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(1222, 686);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(141, 29);
+            this.saveFile.TabIndex = 6;
+            this.saveFile.Text = "Dosyayı Kaydet";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // selectFormat
+            // 
+            this.selectFormat.FormattingEnabled = true;
+            this.selectFormat.Items.AddRange(new object[] {
+            "TXT"});
+            this.selectFormat.Location = new System.Drawing.Point(1015, 691);
+            this.selectFormat.Name = "selectFormat";
+            this.selectFormat.Size = new System.Drawing.Size(147, 24);
+            this.selectFormat.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(1011, 656);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Format Seç:";
+            // 
+            // IslemDefteri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1511, 878);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.selectFormat);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.checkedListBoxAfk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.uploadTxtFile);
+            this.Name = "IslemDefteri";
+            this.Text = "İşlem Defteri(txt)";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadTxtFile;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBoxAfk;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.ComboBox selectFormat;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ MuhasebeFileConverter/IslemDefteri.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MuhasebeFileConverter
+{
+    public partial class IslemDefteri : Form
+    {
+        private DataTable dataTable;
+
+        private string[] headersEnglish;
+        public IslemDefteri()
+        {
+            InitializeComponent();
+            dataTable = new DataTable();
+        }
+
+        private void uploadTxtFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string klasorYolu = folderBrowserDialog.SelectedPath;
+
+                // OpenFileDialog ile dosya seçme işlemi
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Metin Dosyaları (*.txt)|*.txt|Tüm Dosyalar (*.*)|*.*";
+                openFileDialog.Title = "Dosya Seç";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string dosyaYolu = Path.Combine(klasorYolu, openFileDialog.SafeFileName);
+
+                    dataTable.Clear();
+
+                    string[] headersTurkish = File.ReadLines(dosyaYolu).First().Split(';');
+                    foreach (string header in headersTurkish)
+                    {
+                        dataTable.Columns.Add(header, typeof(string));
+                    }
+
+                    headersEnglish = File.ReadLines(dosyaYolu).Skip(1).First().Split(';');
+                    DataRow englishHeadersRow = dataTable.NewRow();
+                    for (int i = 0; i < headersTurkish.Length; i++)
+                    {
+                        englishHeadersRow[i] = headersEnglish[i];
+                    }
+
+                    foreach (string line in File.ReadLines(dosyaYolu).Skip(2))
+                    {
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            string[] rowValues = line.Split(';');
+
+                            // Satırı DataTable'a ekle
+                            dataTable.Rows.Add(rowValues);
+                        }
+                        else
+                        {
+                            DataRow emptyRow = dataTable.NewRow();
+                            dataTable.Rows.Add(emptyRow);
+                        }
+                    }
+
+                    int expectedColumnCount = headersTurkish.Length;
+
+                    foreach (string line in File.ReadLines(dosyaYolu).Skip(2))
+                    {
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            string[] rowValues = line.Split(';');
+
+                            int acigaSatisIndex = Array.FindIndex(headersTurkish, header => header == "ACIGA SATIS ISARETI");
+                            if (acigaSatisIndex != -1)
+                            {
+                                rowValues[acigaSatisIndex] = (rowValues[acigaSatisIndex]);
+                            }
+
+                            int actualColumnCount = rowValues.Length;
+
+                            if (actualColumnCount != expectedColumnCount)
+                            {
+                                MessageBox.Show($"Satır sütun sayısı, DataTable sütun sayısından farklı. Satır: {line}\nBeklenen Sütun Sayısı: {expectedColumnCount}, Gerçek Sütun Sayısı: {actualColumnCount}");
+                            }
+
+                            dataTable.Rows.Add(rowValues);
+                        }
+                        else
+                        {
+                            DataRow emptyRow = dataTable.NewRow();
+                            dataTable.Rows.Add(emptyRow);
+                        }
+                    }
+
+
+                    // DataGridView'a DataTable'ı ata
+                    dataGridView.DataSource = dataTable;
+                    CorrectData();
+                    LoadAfkCodesToCheckedListBox();
+                }
+            }
+        }
+        private void CorrectData()
+        {
+            DataTable correctedDataTable = new DataTable();
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                correctedDataTable.Columns.Add(column.ColumnName, column.DataType);
+            }
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                DataRow correctedRow = correctedDataTable.NewRow();
+
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    string columnName = dataTable.Columns[i].ColumnName;
+                    string originalValue = row[i].ToString();
+
+
+                    if (columnName == "ISLEM KODU")
+                    {
+                        originalValue = originalValue.Replace(".E.", ".E");
+                    }
+
+                    if (columnName == "TAKAS TARIHI")
+                    {
+                        originalValue = originalValue.Replace("; ;", ";;");
+                    }
+
+                    correctedRow[i] = originalValue;
+                }
+
+                correctedDataTable.Rows.Add(correctedRow);
+            }
+
+            dataGridView.DataSource = correctedDataTable;
+        }
+        private void LoadAfkCodesToCheckedListBox()
+        {
+            try
+            {
+                DataColumn afkColumn = dataTable.Columns["ACENTE/FON KODU (AFK)"];
+
+                checkedListBoxAfk.Items.Clear();
+
+                checkedListBoxAfk.Items.Add("Tümünü Göster");
+
+                HashSet<string> uniqueValues = new HashSet<string>();
+                foreach (DataRow row in dataTable.Rows.Cast<DataRow>().Skip(1))
+                {
+                    object value = row[afkColumn];
+                    if (value != DBNull.Value)
+                    {
+                        uniqueValues.Add(value.ToString());
+                    }
+                }
+
+                
+
+
+                foreach (string uniqueValue in uniqueValues)
+                {
+                    checkedListBoxAfk.Items.Add(uniqueValue);
+                }
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluştu: {ex.Message}");
+            }
+        }
+        private void checkedListBoxAfk_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+            List<string> selectedValues = new List<string>();
+            foreach (var item in checkedListBoxAfk.CheckedItems)
+            {
+                selectedValues.Add(item.ToString());
+            }
+
+
+            if (e.NewValue == CheckState.Checked)
+            {
+                string newItem = checkedListBoxAfk.Items[e.Index].ToString();
+                if (!selectedValues.Contains(newItem))
+                {
+                    selectedValues.Add(newItem);
+                }
+            }
+            else
+            {
+                string removedItem = checkedListBoxAfk.Items[e.Index].ToString();
+                selectedValues.Remove(removedItem);
+            }
+
+            UpdateDataTable(selectedValues);
+        }
+
+        private void UpdateDataTable(List<string> selectedValues)
+        {
+            if (selectedValues.Count == 0 || (selectedValues.Count == 1 && selectedValues[0] == "Tümünü Göster"))
+            {
+                dataGridView.DataSource = dataTable;
+            }
+            else
+            {
+                DataView dv = new DataView(dataTable);
+                StringBuilder filterExpression = new StringBuilder();
+                foreach (string value in selectedValues)
+                {
+                    if (filterExpression.Length > 0)
+                    {
+                        filterExpression.Append(" OR ");
+                    }
+                    filterExpression.Append($"[ACENTE/FON KODU (AFK)] = '{value.Replace("'", "''")}'");
+                }
+                dv.RowFilter = filterExpression.ToString();
+                dataGridView.DataSource = dv.ToTable();
+            }
+        }
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = GetFileFilter();
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                switch (selectFormat.SelectedItem.ToString())
+                {
+                    case "TXT":
+                        SaveAsTxt(filePath);
+                        break;
+                }
+
+                MessageBox.Show("Dosya başarıyla kaydedildi.");
+            }
+        }
+
+        private string GetFileFilter()
+        {
+            string selectedFormat = selectFormat.SelectedItem.ToString();
+
+            switch (selectedFormat)
+            {
+                case "TXT":
+                    return "Metin Dosyaları (*.txt)|*.txt";
+                default:
+                    return "Tüm Dosyalar (*.*)|*.*";
+            }
+        }
+
+        private void SaveAsTxt(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Başlıkları yaz
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    writer.Write(dataTable.Columns[i].ColumnName);
+                    if (i < dataTable.Columns.Count - 1)
+                    {
+                        writer.Write(";");
+                    }
+                }
+                
+                writer.WriteLine();
+                for (int i = 0; i < headersEnglish.Length; i++)
+                {
+                    writer.Write(headersEnglish[i]);
+                    if (i < headersEnglish.Length - 1)
+                    {
+                        writer.Write(";");
+                    }
+                }
+                writer.WriteLine();  // Add a newline character after each line
+
+
+
+
+
+                writer.WriteLine(); 
+
+                foreach (DataGridViewRow dgvRow in dataGridView.Rows)
+                {
+                    DataRowView rowView = dgvRow.DataBoundItem as DataRowView;
+                    if (rowView != null)
+                    {
+                        DataRow row = rowView.Row;
+
+                        for (int i = 0; i < dataTable.Columns.Count; i++)
+                        {
+                            writer.Write(row[i].ToString());
+                            if (i < dataTable.Columns.Count - 1)
+                            {
+                                writer.Write(";");
+                            }
+                        }
+
+                        writer.WriteLine(); 
+                    }
+                }
+            }
+        }
+    }
+}
